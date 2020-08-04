c7276601fd9a17a9913a3cdd0f00eb8086099aa8
RomanKhudobei <paul.1998.banks@gmail.com>
2020-08-04 14:06:10 +0300

    Multiprocessing rework

--- new_journal_parser.py
+++ new_journal_parser.py
@@ -4,25 +4,24 @@ import os
 import os.path
 import shutil
 import time
-import re
-import multiprocessing as mp
-from multiprocessing import Pool
+from multiprocessing import Process, Queue
 
 import requests
-from bs4 import BeautifulSoup as BS
+from bs4 import BeautifulSoup
 
 
 def define_soup(url):
-    '''Defines soup object for given url and returns it'''
+    """Defines soup object for given url and returns it"""
     header = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
     }
     response = requests.get(url, headers=header)
-    soup = BS(response.text, 'html.parser')
+    soup = BeautifulSoup(response.text, 'html.parser')
     return soup
 
+
 def extract_volume_links(request_link, path):
-    '''Extracts links to all volumes from response json'''
+    """Extracts links to all volumes from response json"""
     domain = 'http://www.sciencedirect.com'
     header = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
@@ -35,19 +34,19 @@ def extract_volume_links(request_link, path):
     results = {}
     for obj in data:
         volume_name = obj.get('volIssueSupplementText', '')
-        link_to_volume = obj.get('uriLookup', '')
-        link_to_volume = '{}/journal/{}{}'.format(domain, path, link_to_volume)
+        link_to_volume = '{}/journal/{}{}'.format(domain, path, obj.get('uriLookup', ''))
         results.update({volume_name: link_to_volume})
     return results
 
-def get_all_volumes(json_string):
-    '''Gets all volume links'''
-    domain = 'http://www.sciencedirect.com'
-    
-    json_string = json.loads(json_string)     # I don't know why, but loads() returns str...
-    json_data = json.loads(json_string)       # so that's why I'm doing this again
 
-    path = json_data.get('titleMetadata', {}).get('title')      # journal url path-name
+def get_all_volumes(json_string):
+    """Gets all volume links"""
+    domain = 'http://www.sciencedirect.com'
+
+    json_string = json.loads(json_string)  # I don't know why, but loads() returns str...
+    json_data = json.loads(json_string)  # so that's why I'm doing this again
+
+    path = json_data.get('titleMetadata', {}).get('title')  # journal url path-name
 
     data = json_data.get('issuesArchive', {}).get('data', {}).get('results', {})
 
@@ -63,112 +62,115 @@ def get_all_volumes(json_string):
         to_update = extract_volume_links(request_link, path)
         results.update(to_update)
     return results
-        
+
+
 def goto_all_issues(url):
-    '''Returns url to all issues from main journal page'''
-    domain = 'http://www.sciencedirect.com'
+    """Returns url to all issues from main journal page"""
     soup = define_soup(url)
+
     tag = soup.find('a', {'class': 'js-latest-issues-link-text'})
+
+    domain = 'http://www.sciencedirect.com'
     link = tag.get('href')
     link_to_all_volumes = '{}{}'.format(domain, link)
     return link_to_all_volumes
 
+
 def extract_json(url, is_soup=False):
-    '''
+    """
     Extracts json string from given url.
     If is_soup=True - extracts json string from given soup.
-    '''
+    """
+    json_string = ''
+
     if is_soup:
         soup = url
-        tag = soup.find('script', {'type': 'application/json'})
     else:
         soup = define_soup(url)
-        tag = soup.find('script', {'type': 'application/json'})
+
+    tag = soup.find('script', {'type': 'application/json'})
+
     if tag:
-        json_string = tag.text
-        return json_string
-    return None
+        json_string = tag.string
+
+    return json_string
+
 
 def get_journal_name(soup):
-    '''Gets journal name from given soup'''
+    """Gets journal name from given soup"""
     journal_name = soup.find('input', {'name': 'pub'}).get('value')
     return journal_name
 
-def collect_urls_to_parse(url):
-    '''Collects all urls to parse'''
-    urls_to_parse = {}
 
-    link_to_all_volumes = goto_all_issues(url)      # url to page that contain all issues
-    soup = define_soup(link_to_all_volumes)
-    journal_name = get_journal_name(soup).replace(':', '-')
-    
-    json_string = extract_json(soup, is_soup=True)
-    to_parse = get_all_volumes(json_string)     # gets urls to all volumes in journal
+def collect_urls_to_parse(urls, destination_queue):
+    """Collects all urls to parse"""
 
-    urls_to_parse[journal_name] = to_parse
-    return urls_to_parse
+    for url in urls:
+        link_to_all_volumes = goto_all_issues(url)  # url to page that contain all issues
+        soup = define_soup(link_to_all_volumes)
+        journal_name = get_journal_name(soup).replace(':', '-')
 
-def collect_data(data):
-    '''Collects data to single variable after Pool results'''
-    results = {}
-    for info in data:
-        if info:
-            results.update(info)
-    return results
+        json_string = extract_json(soup, is_soup=True)
+        volumes = get_all_volumes(json_string)  # gets urls to all volumes in journal
+
+        destination_queue.put({
+            journal_name: volumes
+        })
+
 
 def except_get_data(data):
-    '''Gets data for exception case'''
-    results = {}
-    for obj in data:    # complex json structure
+    """Gets data for exception case"""
+    for obj in data:  # complex json structure
         issue_sec = obj.get('issueSec', {})
-        for obj2 in issue_sec:
-            items = obj2.get('includeItem', {})
-            for obj3 in items:
-                authors = obj3.get('authors', {})
-                for obj4 in authors:
-                    name = obj4.get('givenName')
-                    surname = obj4.get('surname')
-                    author = '{} {}'.format(name, surname)
-                    emails = obj4.get('emails')
+        for issue in issue_sec:
+            items = issue.get('includeItem', {})
+            for item in items:
+                authors = item.get('authors', {})
+                for author in authors:
+                    name = author.get('givenName')
+                    surname = author.get('surname')
+                    author_name = '{} {}'.format(name, surname)
+                    emails = author.get('emails')
                     if emails:
-                        results[author] = emails
-    return results
+                        yield {author_name: emails}
+
 
 def alternative_get_data(data):
-    '''Tries alternative case to extract needed data'''
-    results = {}
-    for obj in data:    # complex json data structure
+    """Tries alternative case to extract needed data"""
+    for obj in data:  # complex json data structure
         if 'issueSec' in obj:
-            results = except_get_data(data)
-            return results
+            yield except_get_data(data)
+            return
 
-        items = obj.get('includeItem', {}) #or obj.get('issueSec', {})
+        items = obj.get('includeItem', {})  # or obj.get('issueSec', {})
 
-        for obj2 in items:
-            authors = obj2.get('authors')
+        for item in items:
+            authors = item.get('authors')
 
-            for obj3 in authors:
-                name = obj3.get('givenName')
-                surname = obj3.get('surname')
-                author = '{} {}'.format(name, surname)
-                emails = obj3.get('emails')
+            for author in authors:
+                name = author.get('givenName')
+                surname = author.get('surname')
+                author_name = '{} {}'.format(name, surname)
+                emails = author.get('emails')
                 if emails:
-                    results[author] = emails
-    return results
+                    yield {author_name: emails}
 
-def get_data(json_string):
-    '''Gets data from json'''
+
+def parse_journal(url):
+    """Parses given volume of journal"""
+    json_string = extract_json(url)
+
     json_string = json.loads(json_string)
     json_data = json.loads(json_string)
+
     data = json_data.get('articles', {}).get('ihp', {}).get('data', {}).get('issueBody', {}).get('includeItem', [])
 
     if not data:
         data = json_data.get('articles', {}).get('ihp', {}).get('data', {}).get('issueBody', {}).get('issueSec', {})
-        results = alternative_get_data(data)
-        return results
+        yield alternative_get_data(data)
+        return
 
-    results = {}
-    for obj in data:    # complex json data structure
+    for obj in data:  # complex json data structure
         authors = obj.get('authors')
         for obj2 in authors:
             name = obj2.get('givenName')
@@ -176,53 +178,90 @@ def get_data(json_string):
             author = '{} {}'.format(name, surname)
             emails = obj2.get('emails')
             if emails:
-                results[author] = emails
-    return results
+                yield {author: emails}
 
-def parse_journal(volume_name, url):
-    '''Parses given volume of journal'''
-    print('Parsing {}'.format(volume_name))
-    json_string = extract_json(url)
-    data = get_data(json_string)
-    return data
 
-def write_results(filename, results):
-    '''Write results in file'''
-    with open(filename, 'w', encoding='utf-8') as file:
-        for author, emails in results.items():
+def write_results(source_queue):
+    """Write results in file"""
+    prev_journal = None
+    current_file = None
+
+    try:
+        while True:
+            data = source_queue.get()
+
+            filename = '{}.txt'.format(data.get('journal_name'), 'not_found')
+            authors = data.get('author', {})
+
+            if prev_journal != filename:
+
+                if current_file:
+                    current_file.close()
+
+                    shutil.move(
+                        os.path.realpath(prev_journal),
+                        os.path.realpath('Results/{}'.format(prev_journal))
+                    )  # move result file in "Results" directory
+
+                current_file = open(filename, 'w', encoding='utf-8')
+
+            [(author, emails)] = authors.items()
             emails = ', '.join(emails)
-            to_write = '{}; {}\n'.format(author, emails)    # writes csv-like file (.txt) to further import to excel
-            file.write(to_write)
+            to_write = '{}; {}\n'.format(author, emails)  # writes csv-like file (.txt) to further import to excel
+
+            current_file.write(to_write)
+
+            prev_journal = filename
+
+    except KeyboardInterrupt:
+        current_file.close()
+
+        shutil.move(
+            os.path.realpath(prev_journal),
+            os.path.realpath('Results/{}'.format(prev_journal))
+        )
+
+
+def parse_urls(source_queue, destination_queue):
+
+    while True:
+        [(journal_name, volumes)] = source_queue.get().items()
+
+        print('Parsing "{}"'.format(journal_name))
+
+        for volume_name, url in volumes.items():
+            print('\t{}'.format(volume_name))
+            for author in parse_journal(url):
+
+                destination_queue.put({
+                    'journal_name': journal_name,
+                    'author': author
+                })
+
 
 def main():
-    urls_from_file = sys.stdin.read().split()   # takes input
-    urls_from_file = [url.strip() for url in urls_from_file]    # formatizing input
+    urls_from_file = sys.stdin.read().split()  # takes input
+    urls_from_file = [url.strip() for url in urls_from_file]  # formatizing input
 
-    if not os.path.exists('Results'):   # directory to save results files with authors and emails
+    if not os.path.exists('Results'):  # directory to save results files with authors and emails
         os.makedirs('Results')
 
-    print('Collecting urls to parse...')
+    urls_to_parse_queue = Queue()
+    data_to_write_queue = Queue()
 
-    with Pool() as pool:
-        data = pool.map(collect_urls_to_parse, urls_from_file)
+    collect_urls_process = Process(target=collect_urls_to_parse, args=(urls_from_file, urls_to_parse_queue))
+    parse_urls_process = Process(target=parse_urls, args=(urls_to_parse_queue, data_to_write_queue))
+    write_to_file_process = Process(target=write_results, args=(data_to_write_queue,))
 
-    urls_to_parse = collect_data(data)
+    print('Launching processes')
 
-    for journal_name, to_parse in urls_to_parse.items():
-        print('Parsing {}'.format(journal_name))
+    collect_urls_process.start()
+    parse_urls_process.start()
+    write_to_file_process.start()
 
-        params = to_parse.items()
-        with Pool() as pool:
-            data = pool.starmap(parse_journal, params)
-
-        results = collect_data(data)
-
-        filename = '{}.txt'.format(journal_name)
-        write_results(filename, results)
-
-        source = os.path.realpath(filename)
-        destination = os.path.realpath('Results/{}'.format(filename))
-        shutil.move(source, destination)    # move result file in "Results" directory
+    collect_urls_process.join()
+    parse_urls_process.join()
+    write_to_file_process.join()
 
 
 if __name__ == '__main__':
--- parse_journals.py
+++ parse_journals.py
@@ -5,16 +5,19 @@ import requests
 
 
 def is_new_journal(url):
-    '''Checks wether journal is new (UI)'''
+    """Checks whether journal is new (UI)"""
     header = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
     }
+
     response = requests.get(url, headers=header)
     content = response.text
-    is_new_journal = re.findall('Find out more', content)   # the criterion of old/new journal. unique to new journals
-    if is_new_journal:
+
+    if re.findall('Find out more', content):    # the criterion of old/new journal. unique to new journals
         return True
+
     return False
+
 
 def main():
     print('Preparing...')
@@ -22,10 +25,10 @@ def main():
         urls = file.readlines()
         urls = [url.strip() for url in urls]
 
-    new_journals = []   # holds new journals
-    old_journals = []   # holds old journals
+    new_journals = []  # holds new journals
+    old_journals = []  # holds old journals
 
-    for url in urls:    # divide journals to according group
+    for url in urls:  # divide journals to according group
         if is_new_journal(url):
             new_journals.append(url)
         else:
@@ -35,7 +38,7 @@ def main():
     old_journals = '\n'.join(old_journals)  # formatize stdin
 
     if new_journals:
-        new_journal_parser = subprocess.run(['python', 'new_journal_parser.py'], input=new_journals, encoding='utf-8')
+        new_journal_parser = subprocess.run(['python', 'new_journal_parser2.py'], input=new_journals, encoding='utf-8')
 
     # if old_journals:
     #     old_journal_parser = subprocess.run(['python', 'old_journal_parser.py'], input=old_journals, encoding='utf-8')
